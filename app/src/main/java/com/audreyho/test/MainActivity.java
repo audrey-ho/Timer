17cea41d7e36f4bb8fe277ef86892c084b691e2d
Audrey Ho <audreyho@Audreys-MacBook-Pro.local>
2017-06-07 11:29:28 -0400

    Initial commit

---
+++ app/src/main/java/com/audreyho/test/MainActivity.java
@@ -0,0 +1,71 @@
+package com.audreyho.test;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        button = (Button) findViewById(R.id.button1);
+        textView = (TextView) findViewById(R.id.timer);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                CountDownTimer countdown = new CountDownTimer(15*1000, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        textView.setText("Time left: "+millisUntilFinished);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        textView.setText("Time's up!");
+                    }
+                }.start();
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
